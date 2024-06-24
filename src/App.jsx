b79ec39497871d598bd8e3cd19b5dd4d1dc611f4
brian070069 <brianwgatundu@gmail.com>
2024-06-24 15:37:18 +0300

    add pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
-  const name = "brian";
-  console.log(name);
-  return <div className="text-red-600">Real estate</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/singup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignIn = () => {
+  return <div>singin</div>;
+};
+
+export default SignIn;
