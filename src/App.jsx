ed5cf4cb626fcd444699285b1a8b1ac36e1d6f0e
brian070069 <brianwgatundu@gmail.com>
2024-06-24 15:21:12 +0300

    create vite app and install tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+  const name = "brian";
+  console.log(name);
+  return <div className="text-red-600">Real estate</div>;
+};
+
+export default App;
