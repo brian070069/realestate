8e39d7c6a1d6f520f1835829b3e37b44edb09b8b
brian070069 <brianwgatundu@gmail.com>
2024-06-25 21:39:45 +0300

    create login api route

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,5 +1,7 @@
 const User = require("../models/user.model");
-const { hashSync } = require("bcrypt");
+const { hashSync, compare } = require("bcrypt");
+const { handleError } = require("../utils/customError.utils");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res, next) => {
   try {
@@ -13,9 +15,31 @@ const register = async (req, res, next) => {
     next(error);
   }
 };
-const login = (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const isValidUser = await User.findOne({ email }); // check user existence
+    if (!isValidUser) return next(handleError(404, "User does not exist"));
+
+    const userHashedPassword = isValidUser.password;
+    const isValidPassword = await compare(password, userHashedPassword); // validates password
+    if (!isValidPassword) return next(handleError(401, "Invalid credentials"));
+
+    const accessTokens = jwt.sign(
+      { id: isValidUser._id },
+      process.env.JWTSECRET
+    ); //generates jwt
+
+    const { password: pass, ...restOfUserDetails } = isValidUser._doc;
+    res
+      .cookie("accessToken", accessTokens, { httpOnly: true })
+      .status(200)
+      .json(restOfUserDetails);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = { register, login };
